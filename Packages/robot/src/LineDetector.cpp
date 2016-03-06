d5eb2c109d6a49469f3c679a5d6558f1b1870732
atabakhafeez <atabakhafeez@github.com>
2016-03-06 16:37:56 +0000

    Added openCV viewing feature.

--- Packages/robot/src/LineDetector.cpp
+++ Packages/robot/src/LineDetector.cpp
@@ -27,6 +27,8 @@ void callback(const sensor_msgs::LaserScan& msg) {
         }
     }
 
+                           
+
     //convert laser_scan data to image
     float angle = -2.094395;
     for (int i = 0; i < number_of_values; ++i) {
@@ -38,6 +40,9 @@ void callback(const sensor_msgs::LaserScan& msg) {
         //convert to screen coordinates
         int screenx = (int)((cartesianx + screenwidth/2));
         int screeny = (int)((-cartesiany + screenheight/2));
+
+        /*cout << "srceenx" << screenx << endl;
+        cout << "screeny" << screeny << endl;*/
 
         if (screenx > 0 && screeny > 0) {
             image.at<uchar>(screeny, screenx) = (uchar)0;
@@ -51,27 +56,53 @@ void callback(const sensor_msgs::LaserScan& msg) {
 
     }
 
+  /*  namedWindow( "Display window", WINDOW_AUTOSIZE );// Create a window for display.
+    imshow( "Display window", image );                   // Show our image inside it.
+
+    waitKey(0);*/ 
+
+    //cout << "image = "<< endl << " "  << image << endl << endl;
+
     //compute Hough Transform
     cv::Mat dst, cdst;
     cv::Canny(image, dst, 50, 200, 3);
 
-    std::vector<cv::Vec2f> lines;
-    cv::HoughLines(dst, lines, 1, CV_PI/180, 100, 0, 0 );
+    vector<Vec4i> lines;
+    HoughLinesP( dst, lines, 1, CV_PI/180, 100, 30, 10 );
+
+    cv::Mat gray;
+    //cvtColor(image, gray, CV_BGR2GRAY);
+    // smooth it, otherwise a lot of false circles may be detected
+    //GaussianBlur( image, gray, Size(9, 9), 2, 2 );
+    vector<Vec3f> circles;
+    HoughCircles(dst, circles, CV_HOUGH_GRADIENT, 1, dst.rows/8, 200, 100 );
+
+
+    /*std::vector<cv::Vec2f> lines;
+    cv::HoughLines(dst, lines, 1, CV_PI/180, 100, 0, 0 );*/
 
 
 
-    cout << lines.size() << endl;
-    for (int i = 0; i < lines.size(); ++i){
-        cout << (lines.at(i)[0])/100 << endl;
+    cout << "number of lines = " << lines.size() << endl;
+    cout << "number of circles = " << circles.size() << endl;
+    /*for (int i = 0; i < lines.size(); ++i){
+        //cout << (lines.at(i)[0])/100 << endl;
         float scaled_distance = (lines.at(i)[0])/100;
         float originalx = scaled_distance * cos(lines.at(i)[1]) - 5;
         float originaly = scaled_distance * sin(lines.at(i)[1]) - 5;
 
         float distance = sqrt(originalx*originalx + originaly*originaly);
-        cout << "distance = " << distance << endl;
+        //cout << "distance = " << distance << endl;
 
         cout << lines.at(i) << endl;
     }
+
+    for (int i = 0; i < circles.size(); ++i)
+    {
+        cout << circles.at(i) << endl;
+    }*/
+
+
 
 }
 
@@ -79,7 +110,9 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "line_detector");
     ros::NodeHandle n;
 
+    ros::Rate loop_rate(50);
     ros::Subscriber sub = n.subscribe("base_scan", 1000, callback);
     ros::spin();
+    loop_rate.sleep();
     return 0;
 }
